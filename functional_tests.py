23cbfdd9eea6ffe731cb01cc540bf2ad3e457f8e
Nathan Heinrich <nwheinrich@gmail.com>
2018-07-27 00:40:32 -0400

    Func test checks for input item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,18 +18,31 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a to-do item immediately
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
 		# She types "pee in a corner" into a text box
 		# She loves rage peeing like a geriatric cat
+		input_box.send_keys('pee in a corner')
+
 
 		# When Mitsy hits enter, the page updates
 		# Now the page lists "1: pee in a corner" as an item in the to-do list
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(any(row.text == '1: pee in a corner' for row in rows))
 
 		# There is still a text box inviting her to add another item
 		# She enters "projectile vomit kibble on the carpet" 
+		self.fail('Finish the test!')
 
 		# The page updates again, and now shows both items on her list
 
