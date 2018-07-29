5dfdaddd68fe8da3ab947e5d96413d1d09860e6c
Nathan Heinrich <nwheinrich@gmail.com>
2018-07-29 02:09:49 -0400

    func test & refactor

--- functional_tests.py
+++ functional_tests.py
@@ -38,17 +38,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1: pee in a corner' for row in rows), "New to-do item did not appear in table")
+		self.assertIn('1: pee in a corner', [row.text for row in rows])
 
 		# There is still a text box inviting her to add another item
+		input_box = self.browser.find_element_by_id('id_new_item')
+		#self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
 		# She enters "projectile vomit kibble on the carpet" 
-		self.fail('Finish the test!')
+		input_box.send_keys('projectile vomit kibble on the carpet')
+		input_box.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: pee in a corner', [row.text for row in rows])
+		self.assertIn('2: projectile vomit kibble on the carpet', [row.text for row in rows])
 
 		# Mitsy wonders whether the website will remember her list
 		# Then she sees that the site has generated a unique URL for her
 		# There is some explanatory text to that effect
+		self.fail("Finish the test")
 
 		# She visits that URL - her to-do list is still there
 
--- lists/tests.py
+++ lists/tests.py
@@ -15,5 +15,10 @@ class HomePageTest(TestCase):
 		html = response.content.decode('utf8')
 
 		self.assertTemplateUsed(response, 'home.html')
+	def test_can_save_post_request(self):
+
+		response = self.client.post('/', data={'item_text':'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
 
 
