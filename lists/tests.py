cfcb338eb4d8d2b37dad81b0342770d18ca8aede
Nathan Heinrich <nwheinrich@gmail.com>
2018-07-29 14:26:55 -0400

    redirect after POST & show items in template

--- lists/tests.py
+++ lists/tests.py
@@ -16,16 +16,39 @@ class HomePageTest(TestCase):
 		html = response.content.decode('utf8')
 
 		self.assertTemplateUsed(response, 'home.html')
+
+	def test_only_save_items_necessary(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
+
 	def test_can_save_post_request(self):
 
 		response = self.client.post('/', data={'item_text':'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'home.html')
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+	def test_redirects_after_post(self):
+
+		response = self.client.post('/', data={'item_text':'A new list item'})
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_all_list_items_display(self):
+		Item.objects.create(text='item numba one')
+		Item.objects.create(text='item numba two')
+
+		response = self.client.get('/')
+
+		self.assertIn('item numba one', response.content.decode())
+		self.assertIn('item numba two', response.content.decode())
 
 class ItemModelTest(TestCase):
 
 	def test_saving_retrieving_items(self):
-		
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
 		first_item.save()
